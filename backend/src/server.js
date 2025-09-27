328150c2a82e9945b61766d04d9874a6211210b1
aarya-16 <bodasaarya@gmail.com>
2025-09-27 18:53:28 +0530

    Prepared for Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import path from "path";
+
 import authRouter from './routes/auth.route.js';
 import messageRouter from './routes/message.route.js';
+import { log } from 'console';
 
 dotenv.config()
 
 const app = express();
+const __dirname = path.resolve();
+
 const port = process.env.PORT;
 
 app.get('/health', (req, res)=> {
@@ -14,6 +19,14 @@ app.get('/health', (req, res)=> {
 
 app.use('/api/auth', authRouter)
 app.use('/api/message', messageRouter)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
 
 app.listen(port || 3000, () => 
     console.log(`Server started on port ${port}`)
