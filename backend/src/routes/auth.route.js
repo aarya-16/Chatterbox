46aa1ef183938242cae75225e814278f046d8b33
aarya-16 <bodasaarya@gmail.com>
2025-09-30 23:47:56 +0530

    Wrote signup logic

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import { signup } from '../controllers/auth.controllers.js';
 
 const authRouter = express.Router();
 
-authRouter.get('/signup', (req,res) => {
-    res.send("Signup endpoint");
-})
+authRouter.post('/signup', signup)
 
 export default authRouter
